69c6ab50222cb848b26595540eab410b00014377
ivanovdan <dan.b.ivanov@gmail.com>
2018-05-19 02:23:38 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as sources from 'assets/sources.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Bunny Player';
+  showVideo = true;
+  videoSource = (<any>sources).videoSource;
+  audioSource = (<any>sources).audioSource;
+  textSource = (<any>sources).textSource;
+  imageSource = (<any>sources).imageSource;
+
+  abortVideo(event: any) {
+  // Can't use @ViewChild since it's not available
+  const videoPLayer = event.currentTarget.parentElement.parentElement.children[0].children[0];
+    videoPLayer.pause();
+    this.showVideo = !this.showVideo;
+  }
+
+  playAudio(event: any) {
+    const audioPlayer = event.currentTarget.parentElement.children[2];
+    audioPlayer.play();
+  }
 }
